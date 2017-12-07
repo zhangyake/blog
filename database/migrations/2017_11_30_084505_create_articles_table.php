e3f35218cc5cbc43e650941ead7f36495dcb7fbd
jaak <zhangykvip@126.com>
2017-12-07 18:45:01 +0800

    update style

--- database/migrations/2017_11_30_084505_create_articles_table.php
+++ database/migrations/2017_11_30_084505_create_articles_table.php
@@ -13,6 +13,7 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
+        $this->down();
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title')->comment('文章标题');
@@ -20,7 +21,7 @@ class CreateArticlesTable extends Migration
             $table->integer('type_id')->comment('文章类别id');
             $table->longText('content_md')->comment('文章内容');
             $table->longText('content')->comment('文章内容');
-            $table->tinyInteger('is_show')->defalut(1)->comment('文章标题');
+            $table->tinyInteger('is_show')->default('1')->comment('是否显示');
             $table->timestamps();
         });
     }
