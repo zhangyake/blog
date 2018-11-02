412679088e13917ea862f139fc12366311ad147e
jaak <zhangykvip@126.com>
2018-11-02 10:50:05 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 
 Route::get('/', function () {
 //    博客前台页面
-      return view('index');
+      $url = 'https://blog.zhangyake.site';
+      return  redirect('/')->setTargetUrl($url);
+     // return view('index');
 });
 
 Route::get('/chat', function () {
